e110a52c931a7a7c85f8a12ec6dd423f9248bde4
jonathan medina <jonathanmedina3792@gmail.com>
2023-11-09 15:11:31 -0300

    first commit

--- script.js
+++ script.js
@@ -4,23 +4,32 @@ const api_key = 'c51ceabcae4c1131e4c997324f3cedee';
 const urlBase = 'https://api.themoviedb.org/3/search/movie';
 const urlImg = 'http://image.tmdb.org/t/p/w200';
 
-function buscarMovies() {
-    const searchInput = document.getElementById('searchInput').value;
+async function buscarMovies() {
+    try {
+        const searchInput = document.getElementById('searchInput').value;
 
-    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('La solicitud no fue exitosa.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            displayMovies(data.results);
-            guardarBusqueda(searchInput); // Guardar la búsqueda solo si la solicitud fue exitosa
-        })
-        .catch(error => {
-            console.error('Error en la solicitud:', error);
-        });
+        // Usando Fetch para realizar una solicitud GET
+        const fetchResponse = await fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`);
+
+        if (!fetchResponse.ok) {
+            throw new Error('La solicitud Fetch no fue exitosa.');
+        }
+
+        const fetchData = await fetchResponse.json();
+        displayMovies(fetchData.results);
+        guardarBusqueda(searchInput);
+
+        // Usando Axios para realizar una solicitud GET
+        const axiosResponse = await axios.get(`${urlBase}?api_key=${api_key}&query=${searchInput}`);
+
+        if (!axiosResponse.data) {
+            throw new Error('La solicitud Axios no fue exitosa.');
+        }
+
+        displayMovies(axiosResponse.data.results);
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
+    }
 }
 
 function displayMovies(movies) {
@@ -33,26 +42,28 @@ function displayMovies(movies) {
     }
 
     movies.forEach(movie => {
+        const { title, release_date, overview, poster_path } = movie;
+
         const movieDiv = document.createElement('div');
         movieDiv.classList.add('movie');
 
-        const title = document.createElement('h2');
-        title.textContent = movie.title;
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = title;
 
-        const releaseDate = document.createElement('p');
-        releaseDate.textContent = 'La fecha de lanzamiento fue: ' + movie.release_date;
+        const releaseDateElement = document.createElement('p');
+        releaseDateElement.textContent = `La fecha de lanzamiento fue: ${release_date}`;
 
-        const overview = document.createElement('p');
-        overview.textContent = movie.overview;
+        const overviewElement = document.createElement('p');
+        overviewElement.textContent = overview;
 
-        const posterPath = urlImg + movie.poster_path;
+        const posterPath = urlImg + poster_path;
         const poster = document.createElement('img');
         poster.src = posterPath;
 
         movieDiv.appendChild(poster);
-        movieDiv.appendChild(title);
-        movieDiv.appendChild(releaseDate);
-        movieDiv.appendChild(overview);
+        movieDiv.appendChild(titleElement);
+        movieDiv.appendChild(releaseDateElement);
+        movieDiv.appendChild(overviewElement);
 
         resultContainer.appendChild(movieDiv);
     });
